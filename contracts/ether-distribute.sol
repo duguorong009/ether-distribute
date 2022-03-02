b23ca2c5a8f3646583895ae9712298d1bbdf785c
guorong <dudurong009@gmail.com>
2022-03-02 00:42:17 -0700

    Add the function of "user sends the Ether to the contract"

--- contracts/ether-distribute.sol
+++ contracts/ether-distribute.sol
@@ -1,9 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract EtherDistribute {
+    using SafeMath for uint;
 
+    // Variables
     address private admin;
+
+    uint public ethAmount;
+    uint public feeAmount;
+
+    // Events
+    event Received(address, uint);
 
     constructor() {
         admin = msg.sender;
@@ -11,5 +21,10 @@ contract EtherDistribute {
 
     function getAdmin() external view returns (address) {
         return admin;
+    }
+
+    receive() external payable {
+        ethAmount = ethAmount.add(msg.value);
+        emit Received(msg.sender, msg.value);
     }
 }
--- test/ether-distribute.js
+++ test/ether-distribute.js
@@ -13,4 +13,21 @@ describe("Ether-Distribute contract", function() {
 
         expect(contractAdmin.toString()).to.equal(admin.address.toString());
     });
+
+    it("Should receive Ethers from the user", async function () {
+        const [admin, user] = await ethers.getSigners()
+        
+        const ethDistributeFactory = await ethers.getContractFactory("EtherDistribute");
+
+        const ethDistributeContract = await ethDistributeFactory.deploy();
+
+        await user.sendTransaction({
+            to: ethDistributeContract.address,
+            value: ethers.utils.parseEther("1.0")
+        });
+
+        const contractEthAmount = await ethDistributeContract.ethAmount();
+        expect(contractEthAmount.toString()).to.equal(ethers.utils.parseEther("1.0").toString());
+
+    });
 });
